fb9fae4ae67247a775f20bdc2543e2b347d3df12
Carlos Emanuel <carlos.olvr32@gmail.com>
2023-05-13 15:52:05 -0300

    add metodos de imprimir fecho transitivo direto/inverso

--- codigo/src/app/App.java
+++ codigo/src/app/App.java
@@ -5,13 +5,45 @@ import java.io.File;
 
 public class App {
     public static void imprimirFechoTransitivoDiretoFloydWarshall(Grafo g) {
+        boolean fechoTransitivo[][] = FloydWarshall.fechoTransitivo(g);
+        
+        System.out.println("Fecho transitivo direto de todos os vertices do grafo: ");
+        
+        for(int i = 0; i < g.vertices(); i++) {
+            System.out.print("[" + i + "]: ");
+            
+            for(int j = 0; j < g.vertices(); j++) {
+                if(fechoTransitivo[i][j]) {
+                    System.out.print(j + " ");
+                }
+            }
 
+            System.out.println(" ");
+        }
     } 
+    
+    public static void imprimirFechoTransitivoInversoFloydWarshall(Grafo g) {
+        boolean fechoInverso[][] = FloydWarshall.fechoTransitivo(g);
+        System.out.println("Fecho transitivo inverso de todos os vertices do grafo: ");
+
+        for(int i = 0; i < g.vertices(); i++) {
+            System.out.print("["+i+"]: ");
+            for(int j = 0; j < g.vertices(); j++) {
+                if(fechoInverso[j][i]) {
+                    System.out.print(j + " ");
+                }
+            }
+
+            System.out.println(" ");
+        }
+    }
     public static void main(String[] args) {
         Grafo grafo100 = Gerador.gerarGrafoSimples(100, 300);
+        imprimirFechoTransitivoDiretoFloydWarshall(grafo100);
+        imprimirFechoTransitivoInversoFloydWarshall(grafo100);
 
-       /*Gerador.escreverArquivo(grafo100);
-        MetodoNaive.lerArquivo(new File("./codigo/graph100.txt"));*/
+       Gerador.escreverArquivo(grafo100);
+        MetodoNaive.lerArquivo(new File("./codigo/graph100.txt"));
 
 
     }
