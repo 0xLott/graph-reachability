a988b5237a9a01bc81a5196c4aa3ffca1e26eea6
0xLott <llott1710@gmail.com>
2023-05-14 19:18:23 -0300

    Added Contador

--- codigo/src/app/App.java
+++ codigo/src/app/App.java
@@ -1,16 +1,30 @@
 package app;
 
+import java.text.DecimalFormat;
+import java.text.NumberFormat;
+
 import business.*;
 
 
 public class App {
 
     public static void main(String[] args) {
-        Grafo grafo = Gerador.gerarGrafoSimples(15, 30);
-//        Warshall.imprimirFechoTransitivoDireto(grafo);
 
-        MetodoNaive.imprimirFechoTransitivoDireto(grafo);
+        long start = System.currentTimeMillis();    // Início: Contador do tempo de execução 
 
+
+        Grafo grafo = Gerador.gerarGrafoSimples(10000, 1);
+
+        Warshall.imprimirFechoTransitivoDireto(grafo);
+        // Warshall.imprimirFechoTransitivoInverso(grafo);
+
+        // MetodoNaive.imprimirFechoTransitivoDireto(grafo);
+        // MetodoNaive.imprimirFechoTransitivoInverso(grafo);
+
+
+        long end = System.currentTimeMillis();  // Fim: Contador do tempo de execução 
+        NumberFormat formatter = new DecimalFormat("#0.00000");
+        System.out.print("Execution time is " + formatter.format((end - start) / 1000d) + " seconds");      
 
     }
 }
